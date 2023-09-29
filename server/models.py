ad82c0041900d67854d42c200d53451f626a5703
Aruna Turay <tura_arun@yahoo.com>
2023-09-29 17:11:11 -0400

    adds Artist model

--- server/models.py
+++ server/models.py
@@ -4,4 +4,11 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class Artist(db.Model, SerializerMixin):
+    __tablename__ = 'artists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    style = db.Column(db.String)
+    lyrics = db.Column(db.String)
 
