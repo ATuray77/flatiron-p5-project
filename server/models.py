80f70a7d008fcf9b91b87495f77304b926a290b3
Aruna Turay <tura_arun@yahoo.com>
2023-09-29 17:20:10 -0400

    adds user model

--- server/models.py
+++ server/models.py
@@ -8,9 +8,20 @@ class Artist(db.Model, SerializerMixin):
     __tablename__ = 'artists'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    
+
+class Song(db.Model, SerializerMixin):
+    __tablename__ = 'songs'
+
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     style = db.Column(db.String)
     lyrics = db.Column(db.String)
 
-class Songs(db.Model, SerializerMixin):
-    __tablename__ = 'songs'
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    _password_hash = db.Column(db.String)
