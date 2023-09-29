f1a7a13f63d7bd0715284a3ed05592b0a5005126
Aruna Turay <tura_arun@yahoo.com>
2023-09-29 16:54:58 -0400

    edits SongsPage

---
+++ client/src/components/SongsPage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Route, useRouteMatch } from 'react-router-dom'
+import SongsList from './SongsList'
+import SongsShow from './SongsShow'
+//import "./App.css";
+//import SongForm from './SongForm'
+
+
+function SongsPage({ songs, setSongs }) {
+  const match = useRouteMatch();
+  // console.log(match)
+
+
+
+  return (
+    <div>
+       <h3>Choose a song from the list to see details</h3>
+      <SongsList songs={songs} setSongs={setSongs}  />
+      <Route exact path={match.url}>
+       
+      </Route>
+
+      <Route path={`${match.url}/:songID`}>
+        <SongsShow songs={songs} />
+      </Route>
+    </div>
+  );
+}
+
+export default SongsPage
