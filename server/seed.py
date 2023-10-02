20f77ad3c87635af98bfe55e283929414845702f
Aruna Turay <tura_arun@yahoo.com>
2023-10-02 15:13:58 -0400

    seeds completed

--- server/seed.py
+++ server/seed.py
@@ -18,39 +18,38 @@ if __name__ == '__main__':
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
-    Artist.query.delete()
-    Song.query.delete()
-    User.query.delete()
+        Artist.query.delete()
+        Song.query.delete()
+        User.query.delete()
 
 
-print("Creating Artists...")
-    # name
-artist_list = []
-for i in range(10):
-    artist_name = fake.name()
-    artist = Artist(artist_name)
+        print("Creating Artists...")
+        # name
+        artist_list = []
+        for i in range(10):
+            artist = Artist(name=fake.name())
+            artist_list.append(artist)
+        db.session.add_all(artist_list)
+        db.session.commit()
+        # artist_list.append(artist)
 
-db.session.add(artist)
-db.commit()
-artist_list.append(artist)
 
-
+        
+        print ("Creating songs...")
     
-print ("Creating songs...")
-   
-song_style = ["praise", "worship"]
-songs_list = []
-for n in range(20):
-    song = Song(
-        title = fake.sentence(nb_words=5, variable_nb_words=True),
-        style = random.choice(song_style),
-        lyrics = fake.paragraph(nb_sentences=10, variable_nb_sentences=True)
-    )
+        song_style = ["praise", "worship"]
+        songs_list = []
+        for n in range(20):
+            song = Song(
+                title = fake.sentence(nb_words=5, variable_nb_words=True),
+                style = random.choice(song_style),
+                lyrics = fake.paragraph(nb_sentences=10, variable_nb_sentences=True)
+            )
 
 
-    artist = random.choice(artist_list)
-    artist.songs.append(song)
-    songs_list.append(song)
+        artist = random.choice(artist_list)
+        artist.songs.append(song)
+        songs_list.append(song)
 
-    db.session.add_all(songs_list)
-    db.commit()
+        db.session.add_all(songs_list)
+        db.session.commit()
