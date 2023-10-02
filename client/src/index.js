657e8c965fb66113691205d9a546c96d46315c49
Aruna Turay <tura_arun@yahoo.com>
2023-10-02 09:14:04 -0400

    adds SignUp page

--- client/src/index.js
+++ client/src/index.js
@@ -6,3 +6,13 @@ import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
+
+
+// import React from "react";
+// import App from "./components/App";
+// import "./index.css";
+// import { createRoot } from "react-dom/client";
+
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+// root.render(<App />);
