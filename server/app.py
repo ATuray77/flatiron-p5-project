65173825af74130225768e5950c551caff70b3fc
Aruna Turay <tura_arun@yahoo.com>
2023-10-02 23:48:15 -0400

    adds Login class and it's api resouce to app.py

--- server/app.py
+++ server/app.py
@@ -57,6 +57,22 @@ class CheckSession(Resource):
 
         return {}, 204
 
+class Login(Resource):
+
+    def post(self):
+
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': '401 Unauthorized'}, 401
+
 
 
 @app.route('/')
