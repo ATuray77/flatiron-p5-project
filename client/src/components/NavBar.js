15dab17ba56dee285725b713c8d475eee9f18858
Aruna Turay <tura_arun@yahoo.com>
2023-10-02 22:28:52 -0400

    modifies navbar with login and logout logic

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -13,26 +13,55 @@ const linkStyles = {
   color: "white",
 };
 
-function NavBar() {
+function NavBar({ user, setUser }) {
+  function handleLogoutClick() {
+    fetch("/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+      }
+    });
+  }
+
   return (
-    <div>
-      <NavLink to="/signUp" exact style={linkStyles} activeStyle={{ background: "DarkOliveGreen" }}>
-        SignUp
-      </NavLink>
-      <NavLink to="/" exact style={linkStyles} activeStyle={{ background: "DarkOliveGreen" }}>
-        Home
-      </NavLink>
-      <NavLink to="/songs" exact style={linkStyles} activeStyle={{ background: "DarkOliveGreen" }}>
-        Songs
-      </NavLink>
-      <NavLink to="/form" exact style={linkStyles} activeStyle={{ background: "DarkOliveGreen" }}>
-        Form
-      </NavLink>
-      <NavLink to="/userHome" exact style={linkStyles} activeStyle={{ background: "DarkOliveGreen" }}>
-        UserHome
-      </NavLink>
-    </div>
+    <>
+      <div>
+        <NavLink to="/" exact style={linkStyles} activeStyle={{ background: "DarkOliveGreen" }}>
+          Home
+        </NavLink>
+      </div>
+
+      <div>
+        {user ? (
+          <>
+          <button onClick={handleLogoutClick}>Logout</button>
+          <NavLink to="/songs" exact style={linkStyles} activeStyle={{ background: "DarkOliveGreen" }}>
+          Songs
+        </NavLink>
+
+        <NavLink to="/form" exact style={linkStyles} activeStyle={{ background: "DarkOliveGreen" }}>
+          Form
+        </NavLink>
+
+        <NavLink to="/userHome" exact style={linkStyles} activeStyle={{ background: "DarkOliveGreen" }}>
+          UserHome
+        </NavLink>
+        </>
+        ) : (
+        <>
+        <NavLink to="/signUp" exact style={linkStyles} activeStyle={{ background: "DarkOliveGreen" }}>
+          SignUp
+        </NavLink>
+
+        <NavLink to="/login" exact style={linkStyles} activeStyle={{ background: "DarkOliveGreen" }}>
+          Login
+        </NavLink>
+        </>
+       )}
+        
+      </div>
+    </>
   );
 }
+
 
 export default NavBar
